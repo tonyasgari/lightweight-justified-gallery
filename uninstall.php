4f94ce7c74c9eb853dee97336f036aa9ea325576
tonyasgari <tasgari@outlook.com>
2026-09-20 14:31:22 -0700

    Add uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The plugin stores no options or custom data, so there is nothing to remove.
+ *
+ * @package LightweightJustifiedGallery
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
