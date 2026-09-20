9a9181ca0c0be1b7961da4825b95cc0c1a65ed64
tonyasgari <tasgari@outlook.com>
2026-09-20 14:31:24 -0700

    Add block/index.asset.php

---
+++ block/index.asset.php
@@ -0,0 +1,17 @@
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Editor script dependencies and version for the Justified Gallery block.
+ *
+ * @package Lightweight_Justified_Gallery
+ */
+
+return array(
+	'dependencies' => array(
+		'wp-blocks',
+		'wp-block-editor',
+		'wp-components',
+		'wp-element',
+		'wp-i18n',
+	),
+	'version'      => '1.0.0',
+);
