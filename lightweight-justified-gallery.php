180bf6227c86c1686a2b3cdbdfef2de216fff7bb
tonyasgari <tasgari@outlook.com>
2026-09-20 21:43:41 -0700

    Fix drag-and-drop reordering in the editor

--- lightweight-justified-gallery.php
+++ lightweight-justified-gallery.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Lightweight Justified Gallery
  * Description:       A native gallery block with justified automatic and manually composed rows.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 6.5
  * Requires PHP:      7.4
  * Author:            Gallery Block
